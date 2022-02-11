e36fe079741868fce73dfb4dcb3f9a0d99db9991
Joaquin Segovia <joasegovia9427@gmail.com>
2022-02-10 22:31:57 -0300

    inicio de proyecto

---
+++ GameStream/GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Joaquin Segovia on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

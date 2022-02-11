385cd18bedef9a0684fd325cbed896bb5ba88056
Joaquin Segovia <joasegovia9427@gmail.com>
2022-02-10 23:10:05 -0300

    Creando la pantalla principal y ajustando assets

--- GameStream/GameStream/ContentView.swift
+++ GameStream/GameStream/ContentView.swift
@@ -9,13 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+//            Color(red: 21/255, green: 27/255, blue: 53/255, opacity: 1).ignoresSafeArea()
+            Color("BackGround").ignoresSafeArea()
+            
+            VStack{
+                Image("appLogo").resizable().aspectRatio( contentMode: .fit).frame(width: 250)
+                
+                InicioYResgistroView()
+            }
+   
+        }
+  
+        
     }
+}
+
+struct InicioYResgistroView: View{
+    
+    var body: some View{
+        VStack{
+            HStack{
+                Text("INICIA SESION").fontWeight(.bold).foregroundColor(.white)
+                Text("REGISTRATE").fontWeight(.bold).foregroundColor(.white)
+            }
+            
+        }
+        
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        Image("pantalla1").resizable()
         ContentView()
     }
 }
